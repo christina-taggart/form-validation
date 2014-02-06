74ad1ad66ae698ae89fa59b9bd9fabfa5158a23b
Kevin Yeh & Emmanuel Kaunitz <pair+kyeh.mechamanny@devbootcamp.com>
2014-02-06 11:45:39 -0800

    Add validation to prevent submit if errors exist

--- source/form-validator.js
+++ source/form-validator.js
@@ -1,31 +1,68 @@
 // shorthand for $(document).ready();
 $(function(){
   //Your code...
-$("input[name='email']").focusout(function () {
 
-  if ($(this).val().match(/^[a-zA-z0-9\.]+@[a-zA-z0-9]+\.[a-zA-z]+$/) === null){
-    if ($("#email-error").length === 0){
-      $("#errors").append("<li id='email-error'>Email invalid</li>")
+  $("form").on("submit", function(event){
+    event.preventDefault()
+    errors = $('#errors').children()
+    if (errors.length == 0){
+      alert('Submission is OK!')
     }
-  }
-  else {
-    if ($("#email-error").length != 0){
-      $("#email-error").remove()
+    else{
+      var errorString = 'Cannot submit because of the following errors:\n'
+      for(var i = 0; i<errors.length;i++){
+        errorString += '-'+$(errors[i]).text()+'\n'
+      }
+      alert(errorString)
     }
-  }
-})
+  })
+  $("input[name='email']").focusout(function () {
 
+    if ($(this).val().match(/^[a-zA-z0-9\.]+@[a-zA-z0-9]+\.[a-zA-z]+$/) === null){
+      if ($("#email-error").length === 0){
+        $("#errors").append("<li id='email-error'>Email invalid</li>")
+      }
+    }
+    else {
+      if ($("#email-error").length != 0){
+        $("#email-error").remove()
+      }
+    }
+  })
 
+  $("input[name='password']").focusout(function () {
+
+    if ($(this).val().match(/[A-Z]/) === null) {
+      if ($("#password-capital").length === 0) {
+        $("#errors").append("<li id='password-capital'>Password must have a capitAl letter</li>")
+      }
+    }
+    else {
+      if ($("#password-capital").length != 0) {
+        $("#password-capital").remove()
+      }
+    }
+
+    if ($(this).val().match(/[0-9]/) === null) {
+      if ($("#password-number").length === 0) {
+        $("#errors").append("<li id='password-number'>Password must have a number</li>")
+      }
+    }
+    else {
+      if ($("#password-number").length != 0) {
+        $("#password-number").remove()
+      }
+    }
+
+    if ($(this).val().length < 8) {
+      if ($("#password-length").length === 0) {
+        $("#errors").append("<li id='password-length'>Password must have at least 8 characters</li>")
+      }
+    }
+    else {
+      if ($("#password-length").length != 0) {
+        $("#password-length").remove()
+      }
+    }
+  })
 });
-
-
-// When the user clicks the "Sign Up" button
-//     They should be notified if any of the following conditions are NOT true
-//     - The email conforms to the standard pattern
-//     - The password has at least 8 characters
-//     - The password has at least one capital letter
-//     - The password has at least one numeric character
-
-//     If any of the above conditions are false
-//     - The form is not allowed to be submitted
-//     - Error messages are dislpayed
