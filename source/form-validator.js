cc25bd31bb826a2ff788435fbfe7b2dbebad9dd5
Apprentice <apprentice@dbc11.local>
2014-02-06 10:26:24 -0800

    add password validations

--- source/form-validator.js
+++ source/form-validator.js
@@ -1,5 +1,33 @@
 // shorthand for $(document).ready();
 $(function(){
-  //Your code...
-
+  $("#user-form").on('submit', function(e){
+    e.preventDefault();
+    var email = $(this).find("input[name=email]").val();
+    var password = $(this).find("input[name=password]").val();
+    validateEmail(email);
+    validatePassword(password);
+  });
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!re.test(email)){
+      $("#errors").append("<li>Must be a valid emial address</li>");
+    }
+}
+
+function validatePassword(password){
+  var reNum = /\d/;
+  if(!reNum.test(password)){
+    $("#errors").append("<li>Password must have at least one numeric character (0-9)</li>");
+  }
+
+  var reCap = /[A-Z]/;
+  if(!reCap.test(password)){
+   $("#errors").append("<li>Password must have at least one capital letter</li>");
+  }
+
+  if(password.length < 8){
+    $("#errors").append("<li>Password must be at least 8 characters long</li>");
+  }
+}
